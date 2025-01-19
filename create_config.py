c85394c35f6f717c2e5629d001e64eafe780b33a
Sarah rmili <sarahrmili@Sarahs-MacBook-Pro.local>
2025-01-19 13:22:02 +0100

    Finished config creation program and added advertise to intent file

--- create_config.py
+++ create_config.py
@@ -1,8 +1,3 @@
-
-############################################
-# IT WORKS BUT STILL NEEDS SOME ADJUSTMENTS#
-############################################
-
 import json
 import os 
 
@@ -13,8 +8,20 @@ file_path = os.path.join(script_dir, "intent.json")
 with open(file_path, "r") as file:
     intent = json.load(file)
 
-def get_as_number(as_name):
+def get_as_number(as_name): 
     return 1 if as_name == "AS_X" else 2
+
+def get_as_name(as_number):
+    return "AS_X" if as_number == 1 else "AS_Y"
+
+def find_router_name(ip, remote_as, structure=intent):
+    as_name = get_as_name(remote_as)
+    index = 0 if as_name == "AS_X" else 1
+    for router, details in structure['network'][index][get_as_name(remote_as)]['routers'].items():
+        for interface, addr in details['interfaces'].items():
+            if ip == addr.split("/")[0]:
+                return router
+    return None
 
 def create_config(router_name, router_data, as_name):
     config = []
@@ -57,26 +64,45 @@ def create_config(router_name, router_data, as_name):
         config.append(" bgp log-neighbor-changes")
         config.append(" no bgp default ipv4-unicast")
         for ibgp in bgp_config["ibgp"]:
-            config.append(f" neighbor {ibgp['neighbor']} remote-as {get_as_number(ibgp['remote-as'])}")
+            config.append(f" neighbor {ibgp['neighbor']} remote-as {ibgp['remote-as']}")
+            config.append(f" neighbor {ibgp['neighbor']} update-source {ibgp['interface']}")
         if "ebgp" in bgp_config:
             for ebgp in bgp_config["ebgp"]:
-                config.append(f" neighbor {ebgp['neighbor']} remote-as {get_as_number(ebgp['remote-as'])}")
+                config.append(f" neighbor {ebgp['neighbor']} remote-as {ebgp['remote-as']}")
+                config.append(f" neighbor {ebgp['neighbor']} description Connection to {find_router_name(ebgp['neighbor'], ebgp['remote-as'])} in AS {ebgp['remote-as']}")
         config.append("!")
 
 
         config.append("address-family ipv6")
         if "ebgp" in bgp_config:
-            config.append(f" network ") #to change not sure about placement
+            if "advertise" in bgp_config['ebgp'][0]:
+                for net in bgp_config['ebgp'][0]['advertise']:
+                    config.append(f" network {net}") 
             for ebgp in bgp_config["ebgp"]:
                 config.append(f" neighbor {ebgp['neighbor']} activate")
         for ibgp in bgp_config['ibgp']:
             config.append(f" neighbor {ibgp['neighbor']} activate")
+        if "ebgp" in bgp_config:
+            for rule in bgp_config["ebgp"][0]["prefix-lists"]:
+                if rule == "inbound":
+                    config.append(f"neighbor {bgp_config['ebgp'][0]['neighbor']} prefix-list {bgp_config['ebgp'][0]['prefix-lists']['inbound']['name']} in")
+                if rule == "outbound":
+                    config.append(f"neighbor {bgp_config['ebgp'][0]['neighbor']} prefix-list {bgp_config['ebgp'][0]['prefix-lists']['outbound']['name']} out")
         config.append("exit-address-family")
         config.append("!")
     
-    if "ebgp" in bgp_config:
+    if as_name == "AS_Y":
         config.append(f"ipv6 router ospf {router_data['protocols']['process-id']}")
-        config.append(f" router-id {router_data['router-id']}")
+        config.append(f" router-id {router_data['router-id']}\n!")
+    
+
+    if "ebgp" in bgp_config:
+            for rule in bgp_config["ebgp"][0]["prefix-lists"]:
+                if rule == "inbound":
+                    config.append(f"ipv6 prefix-list {bgp_config['ebgp'][0]['prefix-lists']['inbound']['name']} seq {bgp_config['ebgp'][0]['prefix-lists']['inbound']['rules'][0]['seq']} {bgp_config['ebgp'][0]['prefix-lists']['inbound']['rules'][0]['action']} {bgp_config['ebgp'][0]['prefix-lists']['inbound']['rules'][0]['prefix']} ")
+                if rule == "outbound":
+                    config.append(f"ipv6 prefix-list {bgp_config['ebgp'][0]['prefix-lists']['outbound']['name']} seq {bgp_config['ebgp'][0]['prefix-lists']['outbound']['rules'][0]['seq']} {bgp_config['ebgp'][0]['prefix-lists']['outbound']['rules'][0]['action']} {bgp_config['ebgp'][0]['prefix-lists']['outbound']['rules'][0]['prefix']} ")
+
     
     config.append("!\nend")
     return "\n".join(config)
